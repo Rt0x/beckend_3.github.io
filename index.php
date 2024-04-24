e133cd923c3896e5622cf809b6a83698b703ae86
POFFIOLEG <nik.fomenko005@mail.ru>
2024-04-24 17:47:04 +0300

    243у

--- index.php
+++ index.php
@@ -160,46 +160,44 @@
                 </div>
             </form>
         </div>
-        <div class="wrap2 lh-lg font-monospace">
-            <div class="box">
-                <ul class="list-unstyled-sm">
-                    <h5 id="links" class="text-center">Пункты выполненых скриншотов задания-3</h5>
 
-                    <li>
-                        <a>Подключение к общей базе данных;</a>
-                        </br>
-                        <img src="1.png"></img>
-                    </li>
+        <ul class="list-unstyled-sm">
+            <h5 id="links" class="text-center">Пункты выполненых скриншотов задания-3</h5>
 
-                    <li>
-                        <a>Использование личной базы данных</a>
-                        </br>
-                        <img src="2.png"></img>
-                    </li>
-                    <li>
-                        <a>Создание таблиц в базе данных;
-                        </a>
-                        </br>
-                        <img src="3.png"></img>
+            <li>
+                <a>Подключение к общей базе данных;</a>
+                </br>
+                <img src="1.png"></img>
+            </li>
 
-                    <li>
-                        <a>Список таблиц в базе данных ;
-                        </a>
-                        </br>
-                        <img src="4.png"></img>
-                    </li>
-                    <li>
-                        <a>Результат заполнения формы и сохранения результатов в таблицы базы данных;</a>
-                        </br>
-                        <img src="6.png"></img>
-                    </li>
+            <li>
+                <a>Использование личной базы данных</a>
+                </br>
+                <img src="2.png"></img>
+            </li>
+            <li>
+                <a>Создание таблиц в базе данных;
+                </a>
+                </br>
+                <img src="3.png"></img>
 
+            <li>
+                <a>Список таблиц в базе данных ;
+                </a>
+                </br>
+                <img src="4.png"></img>
+            </li>
+            <li>
+                <a>Результат заполнения формы и сохранения результатов в таблицы базы данных;</a>
+                </br>
+                <img src="6.png"></img>
+            </li>
 
 
-                </ul>
-            </div>
-        </div>
+
+        </ul>
     </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
