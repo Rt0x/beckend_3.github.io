78523f5e885cfc38d3259ff4aa803bcf9c821125
POFFIOLEG <oleg.l.log@gmail.com>
2024-04-24 17:38:39 +0300

    1

---
+++ register.php
@@ -0,0 +1,12 @@
+<?php
+require_once ('db.php');
+// require_once ('prob.php');
+$login = $_POST['login'];
+$tel = $_POST['tel'];
+$email = $_POST['email'];
+$date = $_POST['date'];
+$someGroupName = $_POST['someGroupName'];
+$language[] = $_POST['language'];
+$bio = $_POST['bio'];
+$checkt = $_POST['checkt'];
+?>
